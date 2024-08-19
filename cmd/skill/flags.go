834ff65269016bfc57d816a0878e028d5dfcc28f
golovanevvs <lony@inbox.ru>
2024-08-19 22:14:25 +0300

    Добавить работу с переменными окружения

--- cmd/skill/flags.go
+++ cmd/skill/flags.go
@@ -1,10 +1,17 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var flagRunAddr string
 
 func parseFlags() {
 	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
 }
